b6768f783e0e6753e993e190dafb82f3eacb5264
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-05-16 00:01:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ st.set_page_config(layout="wide")
 st.title('Blog AI Assistant')
 st.subheader('Now you can craft perfect blogs with the help of AI')
 
+with st.sidebar:
+    st.title('Input your blog details')
+    st.subheader('Enter details of the blog you want to generate')
+
+    blog_title = st.text_input('Blog Title')
+    keywords = st.text_input('Keywords')
+
+    num_words = st.slider('Number of words', min_value=200, max_value=2500, step=250)
+
+    submit_button = st.button('Generate Blog')
+
