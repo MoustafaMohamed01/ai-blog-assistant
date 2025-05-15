1c2f95dc51240457bffaded61d11e8eb8e680ab4
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-05-16 00:56:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
-import streamlit at st
+import streamlit as st
+import google.generativeai as genai
+from api_key import GEMINI_API_KEY
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+generation_config = {
+    'temperature': 0.9,
+    'top_p': 1,
+    'top_k': 1,
+    'max_output_tokens': 2048,
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
+]
+
+model = genai.GenerativeModel(model_name='gemini-2.0-flash',
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
 
 st.set_page_config(layout="wide")
 st.title('Blog AI Assistant')
@@ -13,5 +35,11 @@ with st.sidebar:
 
     num_words = st.slider('Number of words', min_value=200, max_value=2500, step=250)
 
+    prompt_parts = [f'Generate a comprehensive, engaging blog post relevant to the given title: "{blog_title}" and keywords: "{keywords}". Make sure to incorporate these keywords in the blog post. The blog should be approximately {num_words} words.']
+    response = model.generate_content(prompt_parts)
+
     submit_button = st.button('Generate Blog')
 
+
+if submit_button:
+    st.write(response.text)
